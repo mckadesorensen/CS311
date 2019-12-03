e7d83e9eedcf3cb3bda078cff8d82f3cde2fd0ff
McKade Sorensen <dmsorensen@alaska.edu>
2019-12-02 18:33:33 -0900

    Updated

--- Project_8/cuber_test_suites.cpp
+++ Project_8/cuber_test_suites.cpp
@@ -23,54 +23,66 @@
 // *********************************************************************
 // Test Cases
 // *********************************************************************
-TEST_CASE("Do both const & non-const Cuber objects work")
+// Do both const & non-const Cuber objects work
+TEST_CASE("Const and non-const cuber objects")
 {
     Cuber x;
     const Cuber y;
+    INFO("CONST OR NON-CONST CUBER OBJECT NOT PASSING");
     REQUIRE( x.operator()(2) == 8 );
-    REQUIRE( y.operator()(2) == 8 );
+    REQUIRE( y.operator()(1) == 8 );
 
 
 }
 
-TEST_CASE("Does Cuber work properly for a wide range of values", "[cuber]")
+// Does Cuber work properly for a wide range of values
+TEST_CASE("Range of values", "[cuber]")
 {
     Cuber x;
+    INFO("FAILING ON A RANGE OF VALUES");
     REQUIRE( x.operator()(2) == 8 );
     REQUIRE( x.operator()(3) == 27 );
     REQUIRE( x.operator()(10) == 1000 );
 }
 
-TEST_CASE("Does Cuber work properly for both positive and negative values")
+// Does Cuber work properly for both positive and negative values
+TEST_CASE("Positive and negative arguments")
 {
     Cuber x;
+    INFO("FAILING ON EITHER A NEGATIVE OR POSITIVE VALUE");
     REQUIRE( x.operator()(-2) == -8 );
     REQUIRE( x.operator()(-3) == -27 );
     REQUIRE( x.operator()(2) == 8 );
     REQUIRE( x.operator()(3) == 27 );
 }
 
-TEST_CASE("Does Cuber work properly for both integer and floating-point arguments")
+// Does Cuber work properly for both integer and floating-point arguments
+TEST_CASE("Floating-point and integer arguments")
 {
     Cuber x;
+    INFO("FAILING ON EITHER A FLOAT OR INTEGER VALUE");
     REQUIRE( x.operator()(3.3) == 35.937 );
     REQUIRE( x.operator()(2.25) == 11.390625 );
     REQUIRE( x.operator()(25) == 15625 );
     REQUIRE( x.operator()(30) == 27000 );
 }
 
-TEST_CASE("What about arguments of type char")
+// What about arguments of type cha
+TEST_CASE("Char as arguments")
 {
     Cuber x;
+    INFO("FAILING ON CHAR A VALUE");
     REQUIRE( x.operator()('a') == '!' );
     REQUIRE( x.operator()('0') == 0 );
     REQUIRE( x.operator()('4') == '@' );
     REQUIRE( x.operator()('3') == '+' );
 }
 
-TEST_CASE("Special cases of interest: the cubes of 0, 1, and -1 should be correct")
+// Special cases of interest: the cubes of 0, 1, and -1 should be correct
+TEST_CASE("Special cases")
 {
     Cuber x;
+    INFO("FAILING ON SPECIAL CASES (-1, 0, 1");
     REQUIRE( x.operator()(0) == 0 );
     REQUIRE( x.operator()(1) == 1 );
     REQUIRE( x.operator()(-1) == -1 );
